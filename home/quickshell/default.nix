2cd186b65283425267641a25f5fc4b794ba9d844
Adam Coates <adamcoates175@gmail.com>
2026-03-26 20:42:10 +0100

    test

--- home/quickshell/default.nix
+++ home/quickshell/default.nix
@@ -1,36 +1,11 @@
 { pkgs, inputs, ... }:
 
-let
-  qmlFiles = {
-    "quickshell/shell.qml"          = ./qml/shell.qml;
-    "quickshell/Theme.qml"          = ./qml/Theme.qml;
-    "quickshell/GlobalState.qml"    = ./qml/GlobalState.qml;
-    "quickshell/Bar.qml"            = ./qml/Bar.qml;
-    "quickshell/Workspaces.qml"     = ./qml/Workspaces.qml;
-    "quickshell/Clock.qml"          = ./qml/Clock.qml;
-    "quickshell/Battery.qml"        = ./qml/Battery.qml;
-    "quickshell/Network.qml"        = ./qml/Network.qml;
-    "quickshell/Bluetooth.qml"      = ./qml/Bluetooth.qml;
-    "quickshell/Audio.qml"          = ./qml/Audio.qml;
-    "quickshell/IdleIndicator.qml"  = ./qml/IdleIndicator.qml;
-    "quickshell/NotifServer.qml"    = ./qml/NotifServer.qml;
-    "quickshell/NotifPopup.qml"     = ./qml/NotifPopup.qml;
-    "quickshell/Launcher.qml"       = ./qml/Launcher.qml;
-    "quickshell/ControlCenter.qml"  = ./qml/ControlCenter.qml;
-    "quickshell/AudioSlider.qml"    = ./qml/AudioSlider.qml;
-    "quickshell/BluetoothPanel.qml" = ./qml/BluetoothPanel.qml;
-    "quickshell/NetworkPanel.qml"   = ./qml/NetworkPanel.qml;
-    "quickshell/Wallpaper.qml"      = ./qml/Wallpaper.qml;
-    "quickshell/LockScreen.qml"     = ./qml/LockScreen.qml;
-    "quickshell/PowerMenu.qml"      = ./qml/PowerMenu.qml;
-  };
-in
 {
   home.packages = [
     pkgs.quickshell
   ];
 
-  xdg.configFile = builtins.mapAttrs (name: source: {
-    inherit source;
-  }) qmlFiles;
+  # Deploy the entire qml directory as one unit so all files share
+  # the same Nix store path and QML can resolve sibling types.
+  xdg.configFile."quickshell".source = ./qml;
 }
