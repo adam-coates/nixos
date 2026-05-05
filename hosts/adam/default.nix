4651fa9eefde213de2e2eed835df9f075e5cdb8f
adam-coates <>
2026-05-05 12:17:10 +0200

    ovmf included as default

--- hosts/adam/default.nix
+++ hosts/adam/default.nix
@@ -296,8 +296,6 @@ in
     enable = true;
     qemu = {
       swtpm.enable = true;
-      ovmf.enable = true;
-      ovmf.packages = [ pkgs.OVMFFull.fd ];
       vhostUserPackages = [ pkgs.virtiofsd ];
     };
   };
